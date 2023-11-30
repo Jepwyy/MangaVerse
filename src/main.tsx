608c11e7ce3057d13c0b6da7a26887532d12ccce
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2023-11-30 21:49:45 +0800

    chore: Router Setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ReactRouterProvider from './provider/RouterProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ReactRouterProvider />
+  </React.StrictMode>
 )
---
+++ src/router/router.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const ReactRouter = (children: React.ReactNode, parentPath?: number[]) =>
+  createBrowserRouter(createRoutesFromElements(children, parentPath))
+
+export default ReactRouter
