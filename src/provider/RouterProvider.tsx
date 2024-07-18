5ceaa8ff6d9113bb2e21fe23d74fe0d6f1b433a1
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-07-18 19:19:54 +0800

    feat: vue

--- src/provider/RouterProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from 'react-router-dom'
-import App from '../App'
-
-const ReactRouterProvider = () => {
-  return <RouterProvider router={App} />
-}
-
-export default ReactRouterProvider
