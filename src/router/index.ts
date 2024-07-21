760a54f7d5b9cb33e390c4cd186ebda1e92ca639
Jepwyy <jonelasj.qcydoqcu@gmail.com>
2024-07-21 21:56:44 +0800

    chore: views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
+import InfoView from '../views/InfoView.vue'
+import ReadView from '../views/ReadView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'search',
+      component: SearchView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/info/:id',
+      name: 'info',
+      component: InfoView
+    },
+    {
+      path: '/read/:id',
+      name: 'read',
+      component: ReadView
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
---
+++ src/views/ReadView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>This is an read page</h1>
+  </div>
+</template>
